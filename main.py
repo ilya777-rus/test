a4e24a11745428f2d3bd8ac6a70847b27da15960
ilya777-rus <nickolaeff.ilha@yandex.ru>
2023-04-18 15:48:37 +0500

    #3 добавили функцию для сигнала

---
+++ main.py
@@ -0,0 +1,10 @@
+class Gaming_car:
+    def __init__(self, h: int, w: int):
+        self.w = w
+        self.h = h
+
+    def go(self):
+        return 'go'
+
+    def signal(self):
+        return 'signal'
